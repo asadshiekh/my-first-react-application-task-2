c17c7d00d5cd9a5a448d3f89c5a0b62e912cbd4f
Asad Shiekh <asadshiekh9@gmail.com>
2023-12-25 23:48:09 +0500

    React Single Page Assignment Complete. Added Global Compoents and Slider as well.

--- src/pages/homePage/servicesBlock.js
+++ src/pages/homePage/servicesBlock.js
@@ -1,50 +1,61 @@
-import React from 'react'
-import ServceWebp from '../../assets/images/home/service.webp';
-import ServiceJpg from '../../assets/images/home/service-4.jpg';
+import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+ import ServceWebp from '../../assets/images/home/service.webp';
+ import ServiceJpg from '../../assets/images/home/service-4.jpg';
+import ServicesGlobal from '../../global/components/servicesGlobal';
 function servicesBlock() {
   return (
     <div>
-      <section className="services-block">
-        <div className="container">
-          <div className="services-block-inner-content">
-            <h2>Explore our <span>Services</span></h2>
-            <div className="service-card-section">
-              <div className="service-card-block">
-                <div className="img-wrapper">
-                  <img src={ServceWebp} alt="service" />
-                </div>
-                <p>Ac Services</p>
-              </div>
-              <div className="service-card-block">
-                <div className="img-wrapper">
-                  <img src={ServiceJpg} alt="service" />
-                </div>
-                <p>Ac Services</p>
-              </div>
-              <div className="service-card-block">
-                <div className="img-wrapper">
-                  <img src={ServceWebp} alt="service" />
-                </div>
-                <p>Ac Services</p>
-              </div>
-              <div className="service-card-block">
-                <div className="img-wrapper">
-                  <img src={ServiceJpg} alt="service" />
-                </div>
-                <p>Ac Services</p>
-              </div>
-              <div className="service-card-block">
-                <div className="img-wrapper">
-                  <img src={ServceWebp} alt="service" />
-                </div>
-                <p>Ac Services</p>
-              </div>
+
+      <div className='ourServies page_width'>
+            <h2 className='global_title'>Explore our <span> Services</span> </h2>
+            <div className="card_grid">
+              <Carousel  ssr={true} responsive={responsive} containerClass="carousel-container" itemClass='carouselItem'>
+                  <ServicesGlobal service_img="https://images.pexels.com/photos/4108807/pexels-photo-4108807.jpeg" title='Ac Duct' />
+                  <ServicesGlobal service_img={ServceWebp} service_title="The Title 1"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServiceJpg} service_title="The Title 2"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServceWebp} service_title="The Title 1"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServiceJpg} service_title="The Title 2"></ServicesGlobal>
+                  <ServicesGlobal service_img="https://images.pexels.com/photos/4108807/pexels-photo-4108807.jpeg" title='Ac Duct' />
+                  <ServicesGlobal service_img={ServceWebp} service_title="The Title 1"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServiceJpg} service_title="The Title 2"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServceWebp} service_title="The Title 1"></ServicesGlobal>
+                  <ServicesGlobal service_img={ServiceJpg} service_title="The Title 2"></ServicesGlobal>
+             </Carousel>
             </div>
-          </div>
         </div>
-      </section>
+
     </div>
   )
 }
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+    showDots:false,
+    partialVisibilityGutter: 50,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    showDots:false,
+    partialVisibilityGutter: 50,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 5,
+    showDots:false,
+    partialVisibilityGutter: 50,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    showDots:true,
+    partialVisibilityGutter: false,
+  }
+};
 
 export default servicesBlock
